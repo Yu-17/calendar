a34fd44c00e0222078fb08f6330e675885e86731
Yu-17 <api.0204@gmail.com>
2022-07-23 21:01:48 +0900

    resourse created

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,5 +15,14 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def update
+    @task = Task.find(params[:id])
+    if @task.update_attributes(task_params)
+      redirect_to @task, :notice => 'Task was successfully updated.'
+    else
+      render :action => "edit"
+    end
+  end
   
+      
 end
