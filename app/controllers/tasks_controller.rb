bedfbcac6153dcc7ba0b2c885282f42b72d506bf
Yu-17 <api.0204@gmail.com>
2022-07-29 20:57:16 +0900

    task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,7 +16,19 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
 
+  def update
+    @task = Task.find(params[:id])
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
 
 
   private
