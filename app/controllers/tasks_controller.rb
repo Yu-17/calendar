6559ff6ed7c41efb183fa9bdc3b7a2de5f194bd4
Yu-17 <api.0204@gmail.com>
2022-08-10 20:05:28 +0900

    create destroy

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,12 +12,6 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
-  def create
-    
-    Task.create(task_parameter)
-    redirect_to tasks_path
-  end
-
   def edit
     @task = Task.find(params[:id])
   end
